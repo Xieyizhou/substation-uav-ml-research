f27a4f7cdb6c43a622ca353cb62cca5765b7afc3
Xieyizhou <185841253+Xieyizhou@users.noreply.github.com>
2026-08-19 14:17:28 +0800

    Prepare macOS Beta 0.6.4

--- tests/test_macos_app_contract.py
+++ tests/test_macos_app_contract.py
@@ -22,7 +22,7 @@ class MacOSAppContractTests(unittest.TestCase):
         )
         self.assertEqual(value["CFBundlePackageType"], "APPL")
         self.assertEqual(value["CFBundleIconFile"], "AppIcon")
-        self.assertEqual(value["CFBundleShortVersionString"], "0.6.3")
+        self.assertEqual(value["CFBundleShortVersionString"], "0.6.4")
 
     def test_bundle_version_matches_shared_manifest(self):
         import json
